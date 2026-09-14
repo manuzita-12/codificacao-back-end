8344471d8665f52dacad943604ece147db8b4fbb
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-09-14 20:51:48 -0300

    feat: cria funcao formateLog para formatacao de mensagens com data e hora

---
+++ aula02-modulos-commonjs-esm/utilitario.js
@@ -0,0 +1,7 @@
+export function formateLog(mensagem){
+    const dataAtual = new Date().toISOString().split('T')[0];
+
+    const horaAtual = new Date().toLocaleTimeString();
+
+    return(`[${dataAtual} -  ${horaAtual}]: ${mensagem}`);
+}
