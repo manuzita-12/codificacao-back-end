ba094dd39f88bb6da1de97c06fc6f3b609b2f9ec
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-09-23 21:40:29 -0300

    feat(jogos): criar serviço de mock de dados e busca por ID com tratamento de erro

---
+++ aula10-rotas-dinamicas/src/jogos.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+
+@Injectable()
+export class JogosService {
+    private jogos = [
+        {id: 1, titulo: 'Minecraft', estudio: 'Monjang Studios'},
+        {id: 2, titulo: 'The Legend of Zelda: Ocarina of time', estudio: 'Nintendo'},
+        {id: 3, titulo: 'Grand theft Auto V', estudio: 'Rockstar North'},
+        {id: 4, titulo: 'Elder Ring', estudio: 'FromSoftware'},
+        {id: 5, titulo: 'God of War', estudio: 'Santa Monica Studio'},
+    ];
+
+    buscarPorId(id:number){
+        const jogo = this.jogos.find((j) => j.id ===id);
+        if(!jogo){
+            throw new NotFoundException(`Jogo com ID ${id} não localizado em nosso gromório.`);
+        }
+        return jogo;
+    }
+}
